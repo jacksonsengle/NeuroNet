dd3f38d968cf01f1c88f10e7dcc03ce33910525a
Stephen A. Balaban <stephen@stephenbalaban.com>
2016-07-03 23:36:46 -0700

    Added main server

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # NeuroNet
-A Telnet based cyberpunk RPG
-
+A Telnet based cyberpunk RPG written in Python. (Python 3.)
 
 # Contributors
 Jackson Sengle
 Stephen Balaban
-
---
+++ server/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+    main.py
+    =======
+
+    Main telnet server for NeuroNet.
+
+    :created: 2016-07-03 23:35:23 -0700
+    :copyright: (c) 2016, Lambda Labs, Inc.
+    :license: All Rights Reserved.
+"""
+import telnetlib
